8f616c42f85bf1c1fce2a26d9f3adb688ebe8a86
isomo <jiahaoxiang2000@gmail.com>
2025-04-03 17:27:40 +0800

    chore: Add Manim and ignore generated media files

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from animation!")
+from manim import *
 
 
-if __name__ == "__main__":
-    main()
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
+
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # noqa: F405
+        square.rotate(PI / 4)  # rotate a certain amount noqa
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the circle
+        self.play(FadeOut(square))  # fade out animation
