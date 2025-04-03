17f98b5f4dcf87f38f91184cfa775fe8f8f682ad
isomo <jiahaoxiang2000@gmail.com>
2025-04-03 23:10:30 +0800

    feat: basic sample Add quickstart examples for Manim animation library.
     we want use this to description cipher

--- manim/quickstart.py
+++ manim/quickstart.py
@@ -54,3 +54,220 @@ class TwoTransforms(Scene):
         self.transform()
         self.wait(0.5)  # wait for 0.5 seconds
         self.replacement_transform()
+
+
+class CreatingMobjects(Scene):
+    def construct(self):
+        circle = Circle()
+        self.add(circle)
+        self.wait(1)
+        self.remove(circle)
+        self.wait(1)
+
+
+class Shapes(Scene):
+    def construct(self):
+        circle = Circle()
+        square = Square()
+        triangle = Triangle()
+
+        circle.shift(LEFT)
+        circle.shift(LEFT)
+        square.shift(UP)
+        triangle.shift(RIGHT)
+
+        self.add(circle, square, triangle)
+        self.wait(1)
+
+
+class MobjectPlacement(Scene):
+    def construct(self):
+        circle = Circle()
+        square = Square()
+        triangle = Triangle()
+
+        # place the circle two units left from the origin
+        circle.move_to(LEFT * 2)
+        # place the square to the left of the circle
+        square.next_to(circle, LEFT)
+        # align the left border of the triangle to the left border of the circle
+        triangle.align_to(circle, RIGHT)
+
+        self.add(circle, square, triangle)
+        self.wait(1)
+
+
+class MobjectZOrder(Scene):
+    def construct(self):
+        circle = Circle().shift(LEFT)
+        square = Square().shift(UP)
+        triangle = Triangle().shift(RIGHT)
+
+        circle.set_stroke(color=GREEN, width=20)
+        square.set_fill(YELLOW, opacity=1.0)
+        triangle.set_fill(PINK, opacity=0.5)
+
+        self.add(triangle, square, circle)
+        self.wait(1)
+
+
+class SomeAnimations(Scene):
+    def construct(self):
+        square = Square()
+
+        # some animations display mobjects, ...
+        self.play(FadeIn(square))
+
+        # ... some move or rotate mobjects around...
+        self.play(Rotate(square, PI / 4))
+
+        # some animations remove mobjects from the screen
+        self.play(FadeOut(square))
+
+        self.wait(1)
+
+
+class AnimateExample(Scene):
+    def construct(self):
+        square = Square().set_fill(RED, opacity=1.0)
+        self.add(square)
+
+        # animate the change of color
+        self.play(square.animate.set_fill(WHITE))
+        self.wait(1)
+
+        # animate the change of position and the rotation at the same time
+        self.play(square.animate.shift(UP))
+        self.play(square.animate.rotate(PI / 3))
+        self.wait(1)
+
+
+class Count(Animation):
+    def __init__(
+        self, number: DecimalNumber, start: float, end: float, **kwargs
+    ) -> None:
+        # Pass number as the mobject of the animation
+        super().__init__(number, **kwargs)
+        # Set start and end
+        self.start = start
+        self.end = end
+
+    def interpolate_mobject(self, alpha: float) -> None:
+        # Set value of DecimalNumber according to alpha
+        value = self.start + (alpha * (self.end - self.start))
+        self.mobject.set_value(value)
+
+
+class CountingScene(Scene):
+    def construct(self):
+        # Create Decimal Number and add it to scene
+        number = DecimalNumber().set_color(WHITE).scale(5)
+        # Add an updater to keep the DecimalNumber centered as its value changes
+        number.add_updater(lambda number: number.move_to(ORIGIN))
+
+        self.add(number)
+
+        self.wait()
+
+        # Play the Count Animation to count from 0 to 100 in 4 seconds
+        self.play(Count(number, 0, 100), run_time=4, rate_func=linear)
+
+        self.wait()
+
+
+class MobjectExample(Scene):
+    def construct(self):
+        p1 = [-1, -1, 0]
+        p2 = [1, -1, 0]
+        p3 = [1, 1, 0]
+        p4 = [-1, 1, 0]
+        a = (
+            Line(p1, p2)
+            .append_points(Line(p2, p3).points)
+            .append_points(Line(p3, p4).points)
+        )
+        point_start = a.get_start()
+        point_end = a.get_end()
+        point_center = a.get_center()
+        self.add(
+            Text(f"a.get_start() = {np.round(point_start,2).tolist()}", font_size=24)
+            .to_edge(UR)
+            .set_color(YELLOW)
+        )
+        self.add(
+            Text(f"a.get_end() = {np.round(point_end,2).tolist()}", font_size=24)
+            .next_to(self.mobjects[-1], DOWN)
+            .set_color(RED)
+        )
+        self.add(
+            Text(f"a.get_center() = {np.round(point_center,2).tolist()}", font_size=24)
+            .next_to(self.mobjects[-1], DOWN)
+            .set_color(BLUE)
+        )
+
+        self.add(Dot(a.get_start()).set_color(YELLOW).scale(2))
+        self.add(Dot(a.get_end()).set_color(RED).scale(2))
+        self.add(Dot(a.get_top()).set_color(GREEN_A).scale(2))
+        self.add(Dot(a.get_bottom()).set_color(GREEN_D).scale(3))
+        self.add(Dot(a.get_center()).set_color(BLUE).scale(2))
+        self.add(Dot(a.point_from_proportion(0.5)).set_color(ORANGE).scale(5))
+        self.add(*[Dot(x) for x in a.points])
+        self.add(a)
+
+
+class ExampleRotation(Scene):
+    def construct(self):
+        self.camera.background_color = WHITE
+        m1a = Square().set_color(RED).shift(LEFT)
+        m1b = Circle().set_color(RED).shift(LEFT)
+        m2a = Square().set_color(BLUE).shift(RIGHT)
+        m2b = Circle().set_color(BLUE).shift(RIGHT)
+        points = m2a.points
+        points = np.roll(points, int(len(points) / 4), axis=0)
+        m2a.points = points
+        self.play(Transform(m1a, m1b), Transform(m2a, m2b), run_time=1)
+
+
+class ShowScreenResolution(Scene):
+    def construct(self):
+        pixel_height = config["pixel_height"]  #  1080 is default
+        pixel_width = config["pixel_width"]  # 1920 is default
+        frame_width = config["frame_width"]
+        frame_height = config["frame_height"]
+        self.add(Dot())
+        d1 = Line(frame_width * LEFT / 2, frame_width * RIGHT / 2).to_edge(DOWN)
+        self.add(d1)
+        self.add(Text(str(pixel_width)).next_to(d1, UP))
+        d2 = Line(frame_height * UP / 2, frame_height * DOWN / 2).to_edge(LEFT)
+        self.add(d2)
+        self.add(Text(str(pixel_height)).next_to(d2, RIGHT))
+
+
+class VMobjectDemo(Scene):
+    def construct(self):
+        plane = NumberPlane()
+        my_vmobject = VMobject(color=GREEN)
+        my_vmobject.points = [
+            np.array([-2, -1, 0]),  # start of first curve
+            np.array([-3, 1, 0]),
+            np.array([0, 3, 0]),
+            np.array([1, 3, 0]),  # end of first curve
+            np.array([1, 3, 0]),  # start of second curve
+            np.array([0, 1, 0]),
+            np.array([4, 3, 0]),
+            np.array([4, -2, 0]),  # end of second curve
+        ]
+        handles = [
+            Dot(point, color=RED)
+            for point in [[-3, 1, 0], [0, 3, 0], [0, 1, 0], [4, 3, 0]]
+        ]
+        handle_lines = [
+            Line(
+                my_vmobject.points[ind],
+                my_vmobject.points[ind + 1],
+                color=RED,
+                stroke_width=2,
+            )
+            for ind in range(0, len(my_vmobject.points), 2)
+        ]
+        self.add(plane, *handles, *handle_lines, my_vmobject)
